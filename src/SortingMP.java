0320d7b84ca3264408353fb4d56f33e9c87728b7
Zion Dionisio <zndionisio.2121@gmail.com>
2024-11-16 03:45:55 +0800

    v1.0 - Implemented bubbleSort, insertionSort, and randomizeArray methods.

---
+++ src/SortingMP.java
@@ -0,0 +1,113 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortingMP {
+    private static int[] arrayForSorting;
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String divider = "=========================";
+
+        System.out.print("Enter the size of an array: ");
+        try {
+            int arraySize = userInput.nextInt();
+            arrayForSorting = new int[arraySize];
+            System.out.printf("Enter %d values: ", arraySize);
+
+            for (int i = 0; i < arraySize; i++) {
+                int value = userInput.nextInt();
+                arrayForSorting[i] = value;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+            userInput.nextLine();
+        }
+
+        while (true) {
+            System.out.println(divider);
+            System.out.println("Array Sorting");
+            System.out.println("1. Bubble");
+            System.out.println("2. Insertion");
+            System.out.println("3. Selection");
+            System.out.println("4. Merge Sort");
+            System.out.println("5. Quick Sort");
+            System.out.println("6. Heap Sort");
+            System.out.println("7. Randomize Array");
+            System.out.println("8. Exit");
+            System.out.println("Enter Choice [1 - 8]: ");
+
+            try {
+                int choice = userInput.nextInt();
+                System.out.println(divider);
+                switch (choice) {
+                    case 1 -> bubbleSort(arrayForSorting);
+                    case 2 -> insertionSort(arrayForSorting);
+                    case 7 -> randomizeArray(arrayForSorting);
+                    case 8 -> {
+                        userInput.close();
+                        System.exit(0);
+                    }
+                    default -> System.out.println("Invalid Input");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input");
+                userInput.nextLine();
+            }
+        }
+    }
+
+    static void bubbleSort(int[] array) {
+        int limit = array.length - 1;
+        int temp;
+        for (int i = 0; i < limit; i++) {
+            System.out.printf("Pass %d\n", i + 1);
+            for (int j = 0; j < limit; j++) {
+                System.out.println(Arrays.toString(array));
+                if (array[j] > array[j + 1]) {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
+    }
+
+    static void insertionSort(int[] array) {
+        int limit = array.length;
+        System.out.printf("Current Array: %s\n", Arrays.toString(array));
+
+        for (int i = 1; i < limit; i++) {
+            int temp = array[i];
+            int currentIndex = i - 1;
+
+            System.out.printf("Pass %d ", i);
+            System.out.printf("%s -> ", Arrays.toString(array));
+            while (currentIndex >= 0 && temp < array[currentIndex]) {
+                array[currentIndex + 1] = array[currentIndex];
+                currentIndex--;
+            }
+            array[currentIndex + 1] = temp;
+            System.out.println(Arrays.toString(array));
+        }
+        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
+    }
+
+    static void randomizeArray(int[] array) {
+        int limit = array.length - 1;
+        Random random = new Random();
+
+        System.out.printf("Current Array: %s\n", Arrays.toString(array));
+        for (int i = limit; i > 0; i--) {
+            int upperBound = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[upperBound];
+            array[upperBound] = temp;
+            System.out.printf("%s\n", Arrays.toString(array));
+        }
+        System.out.printf("Randomized Array: %s\n", Arrays.toString(array));
+    }
+}
