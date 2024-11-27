9962c6437ea3f2ebff273fc35d0cb2beb0c69cb7
Zion Dionisio <zndionisio.2121@gmail.com>
2024-11-27 15:50:56 +0800

    Program Finished: Added a few helper methods, Improved code quality, and Updated documentation

--- src/SortingMP.java
+++ src/SortingMP.java
@@ -7,6 +7,7 @@ public class SortingMP {
     private static int[] arrayForSorting;
 
     public static void main(String[] args) {
+        boolean ON = true;
         Scanner userInput = new Scanner(System.in);
         String divider = "=========================";
 
@@ -25,8 +26,8 @@ public class SortingMP {
             userInput.nextLine();
         }
 
-//        While loop ensures that the program does not terminate until chosen so by the user
-        while (true) {
+//        While loop ensures that the program does not terminate until the user chooses to do so
+        while (ON) {
             System.out.println(divider);
             System.out.println("Array Sorting");
             System.out.println("0. Randomize Array");
@@ -37,7 +38,7 @@ public class SortingMP {
             System.out.println("5. Quick Sort");
             System.out.println("6. Heap Sort");
             System.out.println("7. Exit");
-            System.out.print("Enter Choice [1 - 8]: ");
+            System.out.print("Enter Choice [0 - 7]: ");
 
             try {
                 int choice = userInput.nextInt();
@@ -45,24 +46,14 @@ public class SortingMP {
 
                 switch (choice) {
                     case 0 -> randomizeArray(arrayForSorting);
-                    case 1 -> bubbleSort(arrayForSorting);
-                    case 2 -> insertionSort(arrayForSorting);
-                    case 3 -> selectionSort(arrayForSorting);
-                    case 4 -> {
-                        System.out.printf("Current Array: %s\n", Arrays.toString(arrayForSorting));
-                        mergeSort(arrayForSorting);
-                        System.out.printf("Sorted Array: %s\n", Arrays.toString(arrayForSorting));
-                    }
-                    case 5 -> {
-                        System.out.printf("Current Array: %s\n", Arrays.toString(arrayForSorting));
-                        quickSort(arrayForSorting, 0, arrayForSorting.length - 1);
-                        System.out.printf("Sorted Array: %s\n", Arrays.toString(arrayForSorting));
-                    }
-                    case 6 -> heapSort(arrayForSorting);
-                    case 7 -> {
-                        userInput.close();
-                        System.exit(0);
-                    }
+                    case 1 -> displaySort(arrayForSorting, () -> bubbleSort(arrayForSorting));
+                    case 2 -> displaySort(arrayForSorting, () -> insertionSort(arrayForSorting));
+                    case 3 -> displaySort(arrayForSorting, () -> selectionSort(arrayForSorting));
+                    case 4 -> displaySort(arrayForSorting, () -> mergeSort(arrayForSorting));
+                    case 5 -> displaySort(arrayForSorting, () ->
+                            quickSort(arrayForSorting, 0, arrayForSorting.length - 1));
+                    case 6 -> displaySort(arrayForSorting, () -> heapSort(arrayForSorting));
+                    case 7 -> ON = false;
                     default -> System.out.println("Invalid Input");
                 }
             } catch (InputMismatchException e) {
@@ -70,12 +61,18 @@ public class SortingMP {
                 userInput.nextLine();
             }
         }
+        userInput.close();
+    }
+
+    //    Helper method to simplify display of sorting output
+    private static void displaySort(int[] array, Runnable sortingMethod) {
+        System.out.printf("Initial Array: %s\n", Arrays.toString(array));
+        sortingMethod.run();
+        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
     }
 
     static void bubbleSort(int[] array) {
         int limit = array.length - 1;
-        int temp;
-        System.out.printf("Current Array: %s\n", Arrays.toString(array));
 
 //        Outer loop to manage the number of iterations to sort the array
         for (int i = 0; i < limit; i++) {
@@ -83,21 +80,14 @@ public class SortingMP {
 //            Inner loop to compare adjacent elements and swap them if needed
             for (int j = 0; j < limit; j++) {
                 System.out.println(Arrays.toString(array));
-//                Compare adjacent elements to determine if swapping is required
-                if (array[j] > array[j + 1]) {
-//                    If the current element is bigger, swap it with the next
-                    temp = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = temp;
-                }
+//                    If the current element is bigger, swap it with the next element
+                if (array[j] > array[j + 1]) swap(array, j, j + 1);
             }
         }
-        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
     }
 
     static void insertionSort(int[] array) {
         int limit = array.length;
-        System.out.printf("Current Array: %s\n", Arrays.toString(array));
 
 //        Outer loop to manage the number of iterations to sort the array
         for (int i = 1; i < limit; i++) {
@@ -114,33 +104,25 @@ public class SortingMP {
             array[currentIndex + 1] = temp;
             System.out.println(Arrays.toString(array));
         }
-        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
     }
 
     static void selectionSort(int[] array) {
         int limit = array.length;
-        int temp;
 
-        System.out.printf("Current Array: %s\n", Arrays.toString(array));
 //        Outer loop to manage the number of iterations to sort the array
         for (int i = 0; i < limit; i++) {
             int minIndex = i;
             System.out.printf("Pass %d ", i + 1);
 //            Inner loop to handle the search for the minimum element
             for (int j = i; j < limit; j++) {
-                if (array[j] < array[minIndex]) {
-//                    if the current element is smaller than the minimum element
-                    minIndex = j; // The current element is the new minimum
-                }
+//                Compare current and minimum element. If current is smaller, it replaces the minimum
+                if (array[j] < array[minIndex]) minIndex = j;
             }
 
 //            Swap the positions of the current element and the minimum element
-            temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
+            swap(array, i, minIndex);
             System.out.println(Arrays.toString(array));
         }
-        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
     }
 
     static void mergeSort(int[] array) {
@@ -234,90 +216,81 @@ public class SortingMP {
     private static int partition(int[] array, int start, int end) {
         int pivot = array[end];
         int i = start - 1;
-        int temp;
 
 //        Loop through an array and partition it around the pivot
         for (int j = start; j <= end - 1; j++) {
             if (array[j] < pivot) {
                 i++;
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                swap(array, i, j);
             }
         }
 //        Place the pivot in its correct position
         i++;
-        temp = array[i];
-        array[i] = array[end];
-        array[end] = temp;
-
+        swap(array, i, end);
         return i; // return the index of the pivot
     }
 
     static void heapSort(int[] array) {
         int arrayLength = array.length;
-        int temp;
-
-        System.out.printf("Current Array: %s\n", Arrays.toString(array));
 
 //        Loop that builds the max heap
-        System.out.println("Building max heap:");
         for (int i = arrayLength / 2 - 1; i >= 0; i--) {
             heapify(array, arrayLength, i);
         }
-        System.out.printf("Heap after heapify: %s\n", Arrays.toString(array));
+        System.out.printf("Max heap: %s\n\n", Arrays.toString(array));
 
 //        Loop extracts from the heap one by one, sorting the array in ascending order
         System.out.println("Sorting Array:");
         for (int i = arrayLength - 1; i > 0; i--) {
-            temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
+            swap(array, 0, i);
 
-            System.out.printf("After swapping root with index %d: %s\n", i, Arrays.toString(array));
+            System.out.printf("Swapped root(%d) with %d[index %d]: %s\n",
+                    array[0],
+                    array[i], i,
+                    Arrays.toString(array));
 
 //            heapify() helper method maintains the max heap property
             heapify(array, i, 0);
-            System.out.printf("Heap after heapify: %s\n", Arrays.toString(array));
+            System.out.printf("Heapify: %s\n", Arrays.toString(array));
         }
-        System.out.printf("Sorted Array: %s\n", Arrays.toString(array));
     }
 
     //    Helper Method for heapSort()
     private static void heapify(int[] array, int size, int root) {
         int largest = root;
-        int temp;
+
         while (true) {
             int left = 2 * root + 1;
             int right = 2 * root + 2;
-
 //            Compare if either the left or right children is larger than the root
-            largest = left < size && array[left] > array[largest] ? left : largest;
-            largest = right < size && array[right] > array[largest] ? right : largest;
+            if (left < size && array[left] > array[largest]) largest = left;
+            if (right < size && array[right] > array[largest]) largest = right;
 
             if (largest == root) break;
 
 //            Swap the root with the largest child
-            temp = array[root];
-            array[root] = array[largest];
-            array[largest] = temp;
-
+            swap(array, root, largest);
             root = largest;
         }
+    }
+
+    //    Simple helper method for swapping elements
+    private static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 
     static void randomizeArray(int[] array) {
         int limit = array.length - 1;
         Random random = new Random();
 
-        System.out.printf("Current Array: %s\n", Arrays.toString(array));
+        System.out.printf("Initial Array: %s\n", Arrays.toString(array));
 //        Iterate over an array starting from the last element
         for (int i = limit; i > 0; i--) {
 //            Swap the position of the current element and a random element to its left
             int upperBound = random.nextInt(i + 1);
-            int temp = array[i];
-            array[i] = array[upperBound];
-            array[upperBound] = temp;
+            swap(array, i, upperBound);
         }
         System.out.printf("Randomized Array: %s\n", Arrays.toString(array));
     }
